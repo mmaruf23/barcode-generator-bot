06f6898cee8ff59adf339a42bad64add69eb7274
mmaruf <maruuf82@gmail.com>
2025-10-09 21:40:40 +0700

    fix error kotak-kotak text?

--- src/services/barcode.ts
+++ src/services/barcode.ts
@@ -1,6 +1,23 @@
-import { createCanvas } from 'canvas';
+import { createCanvas, registerFont } from 'canvas';
 import JsBarcode from 'jsbarcode';
+import path from 'node:path';
 
+const fontpath = path.join(
+  process.cwd(),
+  'assets',
+  'fonts',
+  'RobotoMono-Regular.ttf'
+);
+
+const customFontFamilyName = 'MyRoboto';
+
+try {
+  registerFont(fontpath, {
+    family: customFontFamilyName,
+  });
+} catch (error) {
+  console.warn('failed register font ', error);
+}
 export async function generateBarcodes(text: string) {
   const texts = text.split(/[,\s]+/).slice(0, 10);
 
@@ -9,7 +26,11 @@ export async function generateBarcodes(text: string) {
 
   const barcodes = texts.map((t) => {
     const canvas = createCanvas(400, 150);
-    JsBarcode(canvas, t, { format: 'CODE128', text: t });
+    JsBarcode(canvas, t, {
+      format: 'CODE128',
+      text: t,
+      font: customFontFamilyName,
+    });
     const buffer = canvas.toBuffer('image/png');
     return buffer;
   });
